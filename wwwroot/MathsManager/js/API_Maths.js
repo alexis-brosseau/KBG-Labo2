6cbc88ae521dda9d305e36ef92a2770ce1e24829
julie-beaucage <201386196@edu.clg.qc.ca>
2024-10-03 10:15:23 -0400

    api??

--- wwwroot/MathsManager/js/API_Maths.js
+++ wwwroot/MathsManager/js/API_Maths.js
@@ -1,47 +1,28 @@
-//const API_URL = "https://api-server-5.glitch.me/api/contacts";
-const API_URL = "http://localhost:5000/api/contacts";
+const API_URL = "http://localhost:5000/api/maths";
 let currentHttpError = "";
 
-function API_getcurrentHttpError () {
-    return currentHttpError; 
+function API_getcurrentHttpError() {
+    return currentHttpError;
 }
-function API_GetContacts() {
+
+function API_RunTest(test) {
     return new Promise(resolve => {
+        const params = new URLSearchParams(test).toString();
         $.ajax({
-            url: API_URL,
-            success: contacts => { currentHttpError = ""; resolve(contacts); },
-            error: (xhr) => { console.log(xhr); resolve(null); }
+            url: `${API_URL}?${params}`,
+            success: result => { 
+                currentHttpError = ""; 
+                resolve(result); 
+            },
+            error: (xhr) => { 
+                console.log(xhr); 
+                currentHttpError = xhr.responseJSON.error_description || "An error occurred";
+                resolve(null); 
+            }
         });
     });
 }
-function API_GetContact(contactId) {
-    return new Promise(resolve => {
-        $.ajax({
-            url: API_URL + "/" + contactId,
-            success: contact => { currentHttpError = ""; resolve(contact); },
-            error: (xhr) => { currentHttpError = xhr.responseJSON.error_description; resolve(null); }
-        });
-    });
-}
-function API_SaveContact(contact, create) {
-    return new Promise(resolve => {
-        $.ajax({
-            url: create ? API_URL :  API_URL + "/" + contact.Id,
-            type: create ? "POST" : "PUT",
-            contentType: 'application/json',
-            data: JSON.stringify(contact),
-            success: (/*data*/) => { currentHttpError = ""; resolve(true); },
-            error: (xhr) => {currentHttpError = xhr.responseJSON.error_description; resolve(false /*xhr.status*/); }
-        });
-    });
-}
-function API_DeleteContact(id) {
-    return new Promise(resolve => {
-        $.ajax({
-            url: API_URL + "/" + id,
-            type: "DELETE",
-            success: () => { currentHttpError = ""; resolve(true); },
-            error: (xhr) => { currentHttpError = xhr.responseJSON.error_description; resolve(false /*xhr.status*/); }
-        });
-    });
+
+function API_RunAllTests(tests) {
+    return Promise.all(tests.map(test => API_RunTest(test)));
 }
